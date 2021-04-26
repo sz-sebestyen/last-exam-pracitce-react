8f420d3ba598cbc8bf0f94a3065de7bbe16236b6
Sebestyen Szabo <szabosebestyen.mail@gmail.com>
2021-04-26 19:31:59 +0200

    Add post request

--- src/components/Pet/Pet.jsx
+++ src/components/Pet/Pet.jsx
@@ -2,9 +2,37 @@ import React, { useState } from "react";
 
 function Pet(props) {
   const [vaccinated, setVaccinated] = useState(props.pet.isVaccinated);
+  const [status, setStatus] = useState("show");
+
+  const toggleVaccinationStatus = async () => {
+    setStatus("fetching");
+
+    fetch("/api/pets/", {
+      method: "post",
+      body: JSON.stringify({
+        name: props.pet.name,
+        isVaccinated: !vaccinated,
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          setVaccinated((prev) => !prev);
+          setStatus("show");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div>
-      {props.pet.name} - Vaccinated: <button>{}</button>
+      {props.pet.name} - Vaccinated:{" "}
+      <button type="button" onClick={() => toggleVaccinationStatus()}>
+        {status === "show" ? (vaccinated ? "true" : "false") : "..."}
+      </button>
     </div>
   );
 }
